e7637b9c32b871f35c9135f2290b6a1f3f37c0b0
Steve Lawrence <slawrence@owlcyberdefense.com>
2023-06-15 09:18:02 -0400

    Update to Daffodil 3.5.0

--- hexWords/build.sbt
+++ hexWords/build.sbt
@@ -7,9 +7,9 @@ version := "0.0.1"
 scalaVersion := "2.12.15"
 
 libraryDependencies ++= Seq(
-  "org.apache.daffodil" %% "daffodil-sapi" % "3.2.1",
-  "org.apache.daffodil" %% "daffodil-tdml-processor" % "3.2.1" % "test",
-  "org.apache.daffodil" %% "daffodil-japi" % "3.2.1",
+  "org.apache.daffodil" %% "daffodil-sapi" % "3.5.0",
+  "org.apache.daffodil" %% "daffodil-tdml-processor" % "3.5.0" % "test",
+  "org.apache.daffodil" %% "daffodil-japi" % "3.5.0",
   "junit" % "junit" % "4.13.2" % "test",
   "com.github.sbt" % "junit-interface" % "0.13.2" % "test"
 )
--- xslt-csv/src/test/java/TestXSLTCSV.java
+++ xslt-csv/src/test/java/TestXSLTCSV.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.PrintStream;
 
 public class TestXSLTCSV {
@@ -10,7 +11,10 @@ public class TestXSLTCSV {
       ByteArrayOutputStream myOut = new ByteArrayOutputStream();
       System.setOut(new PrintStream(myOut));
 
-      String[] args = {"src/test/resources/Dummy.xml", "src/main/resources/processCSV.xsl"};
+      File dummyXML = new File(this.getClass().getResource("Dummy.xml").toURI());
+      File csvXSL = new File(this.getClass().getResource("processCSV.xsl").toURI());
+
+      String[] args = {dummyXML.toString(), csvXSL.toString()};
       net.sf.saxon.Transform.main(args);
 
       String standardOutput = myOut.toString();
